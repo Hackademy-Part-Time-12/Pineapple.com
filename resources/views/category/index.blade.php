bcd5936940fb8195f6658d6b2578c350fcfa9be5
TheObscureKing <theobscureking90@gmail.com>
2023-11-24 18:09:39 +0100

    Carousel funzionante

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -8,7 +8,7 @@
             <div class="col-12 col-md-6 col-lg-4 pt-5 text-center">
 
                 <div class="imageDimension card mb-3">
-                    <img src="{{ $category->cover }}" class="imageDimension img-fluid card-img-top" alt="...">
+                    <img src="{{ $category->cover }}" class="imageDimension img-fluid card-img-top" alt="{{ $category->name }}">
                     <div class="card-body">
                         <a href="{{route('category.show', compact('category'))}}"><h5 class="card-title">{{ $category->name }}</h5></a>
                     </div>
--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -8,7 +8,32 @@
     <div class="container my-5">
         <div class="row justify-content-center">
             <div class="col-12 col-md-6">
-                <img class="img-fluid" src="{{Storage::url($product->cover)}}" alt="">
+                <div id="carouselExampleIndicators" class="carousel slide">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="{{Storage::url($product->cover)}}" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="{{Storage::url($product->cover)}}" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="{{Storage::url($product->cover)}}" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
             </div>
             <div class="col-12 col-md-6">
                 <div class="col-12 font-color-dark ">
