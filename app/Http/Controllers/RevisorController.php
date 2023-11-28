82aca9e830ac7745b8754563a0d1838725f8c2c4
Nicolò Ayroldi <ayrnic90@gmail.com>
2023-11-28 21:19:00 +0100

    creata direttiva blade become_revisor

--- app/Http/Controllers/RevisorController.php
+++ app/Http/Controllers/RevisorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\Product;
+use App\Mail\BecomeRevisor;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
