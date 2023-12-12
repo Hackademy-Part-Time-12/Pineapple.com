a440a63d2a564db8893f563b5ef441918120a717
Fabrizio Mattone <fabriziomattone1999@gmail.com>
2023-12-12 21:49:16 +0100

    aggiunto googleVisionLableImage

--- app/Livewire/ProductCreateForm.php
+++ app/Livewire/ProductCreateForm.php
@@ -6,6 +6,7 @@ use App\Models\Product;
 use Livewire\Component;
 use App\Models\Category;
 use App\Jobs\ResizeImage;
+use App\Jobs\GoogleVisionLabelImage;
 use App\Jobs\GoogleVisionSafeSearch;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Features\SupportFileUploads\WithFileUploads;
@@ -84,6 +85,7 @@ class ProductCreateForm extends Component
 
                 dispatch(new ResizeImage($newImage->path , 400 , 300));
                 dispatch(new GoogleVisionSafeSearch($newImage->id));
+                dispatch(new GoogleVisionLabelImage($newImage->id));
             }
 
             //File::deleteDirectory(storage_path('/app/livewire-tmp'));
