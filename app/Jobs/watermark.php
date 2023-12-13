7d3687de33c5909dca31012a6ec65bcea51d5017
Fabrizio Mattone <fabriziomattone1999@gmail.com>
2023-12-13 20:45:23 +0100

    implementato watermark

---
+++ app/Jobs/watermark.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Jobs;
+
+use Spatie\Image\Image as SpatieImage;
+use App\Models\Image; 
+use Illuminate\Bus\Queueable;
+use Spatie\Image\Manipulations;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+
+class watermark implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+
+    private $product_image_id;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($product_image_id)
+    {
+        $this->product_image_id = $product_image_id;
+    }
+
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+
+        $i = Image::find($this->product_image_id);
+        if (!$i){
+            return;
+        }
+
+        $srcPath = storage_path('app/public/' . $i->path);
+        $image = file_get_contents($srcPath);
+
+
+        $image = SpatieImage::load($srcPath)
+
+                      ->watermarkPosition(Manipulations::POSITION_CENTER)
+                      ->watermarkPadding(10, 10, Manipulations::UNIT_PERCENT) // 10% padding around the watermark
+                      ->watermarkOpacity(50);
+
+                $image->save($srcPath);
+                      
+
+    }
+}
--- app/Livewire/ProductCreateForm.php
+++ app/Livewire/ProductCreateForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Jobs\watermark;
 use App\Models\Product;
 use Livewire\Component;
 use App\Models\Category;
@@ -10,6 +11,7 @@ use App\Jobs\ResizeImage;
 use App\Jobs\GoogleVisionLabelImage;
 use App\Jobs\GoogleVisionSafeSearch;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 use Livewire\Features\SupportFileUploads\WithFileUploads;
 
 class ProductCreateForm extends Component
@@ -84,12 +86,17 @@ class ProductCreateForm extends Component
                 $newFileName = "products/".$this->product->id;
                 $newImage = $this->product->images()->create(['path' => $image->store($newFileName, 'public')]);
 
-            RemoveFaces::withChain([
+           /* RemoveFaces::withChain([
                 new ResizeImage($newImage->path , 400 , 300),
                 new GoogleVisionSafeSearch($newImage->id),
-                new GoogleVisionLabelImage($newImage->id)
+                new GoogleVisionLabelImage($newImage->id),
+                new watermark($newImage->id),
             ])->dispatch($newImage->id);
-                
+            */
+
+                dispatch ( new ResizeImage($newImage->path , 400 , 300));
+                dispatch (new watermark($newImage->id));
+            
             }
 
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
