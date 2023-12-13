e7942e133cb941deb8ef982f5a17a93b12a5bbf2
Fabrizio Mattone <fabriziomattone1999@gmail.com>
2023-12-13 20:05:08 +0100

    applicazione censura volti

--- app/Livewire/ProductCreateForm.php
+++ app/Livewire/ProductCreateForm.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use App\Models\Product;
 use Livewire\Component;
 use App\Models\Category;
+use App\Jobs\RemoveFaces;
 use App\Jobs\ResizeImage;
 use App\Jobs\GoogleVisionLabelImage;
 use App\Jobs\GoogleVisionSafeSearch;
@@ -83,12 +84,15 @@ class ProductCreateForm extends Component
                 $newFileName = "products/".$this->product->id;
                 $newImage = $this->product->images()->create(['path' => $image->store($newFileName, 'public')]);
 
-                dispatch(new ResizeImage($newImage->path , 400 , 300));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
+            RemoveFaces::withChain([
+                new ResizeImage($newImage->path , 400 , 300),
+                new GoogleVisionSafeSearch($newImage->id),
+                new GoogleVisionLabelImage($newImage->id)
+            ])->dispatch($newImage->id);
+                
             }
 
-            //File::deleteDirectory(storage_path('/app/livewire-tmp'));
+            File::deleteDirectory(storage_path('/app/livewire-tmp'));
         }
         session()->flash('productCreated', 'Annuncio inserito con successo');
             $this->cleanForm();
